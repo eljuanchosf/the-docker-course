3ed18dfc57844901d25aea980f77ed803ba9f7d3
Juan Pablo Genovese <juanpgenovese@gmail.com>
2017-01-06 12:39:34 -0300

    Add comment for clarification

--- hello-world.sh
+++ hello-world.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+
+# Create the container by downloading the busybox Linux image and execute the command '/bin/echo "Hello world"'
+# then automatically remove the container (--rm) when exits.
 docker run --rm busybox:latest /bin/echo "Hello world"
